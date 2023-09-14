fd93ee680a550a67ae4c80434f44b89f9245f7a8
Ashish Sapkota <aacsspkt3@gmail.com>
2023-09-14 16:51:56 +0545

    add quick sort algorithm

---
+++ src/quick_sort.rs
@@ -0,0 +1,46 @@
+
+#[allow(dead_code)]
+pub fn quick_sort<T: PartialOrd + Clone>(array: &mut [T]) {
+    if array.is_empty() {
+        return;
+    }
+
+    let pivot = partition(array);
+    let len = array.len();
+
+    quick_sort(&mut array[0..pivot]);
+    quick_sort(&mut array[pivot + 1..len]);
+}
+
+fn partition<T: PartialOrd + Clone>(array: &mut [T]) -> usize {
+    let len = array.len();
+    let pivot = array[len - 1].clone();
+
+    let mut i = 0;
+    let mut j = 0;
+
+    while j < len - 1 {
+        if array[j] <= pivot {
+            array.swap(i, j);
+            i += 1;
+        }
+        j += 1
+    }
+
+    array.swap(i, len - 1);
+    
+    i
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_quick_sort() {
+        let mut v = vec![4,6,1,8,11,13,3];
+        
+        quick_sort(&mut v);
+        assert_eq!(v, vec![1,3,4,6,8,11,13]);
+    }
+}
